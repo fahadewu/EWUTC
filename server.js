d392e319cc3c3b7e1d15be1f77d7beda44834ce6
fahadewu <2021-1-50-002@std.ewubd.edu>
2023-08-08 05:01:03 +0600

    added cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const Routes = require('./routes/userRoutes.js');
 const app = express();
-
+const cors = require('cors');
 app.use(express.static('public'));
 
 app.use('/', Routes);
+app.use(cors("https://dev.ewutc.com"));
+
 
 app.listen();
