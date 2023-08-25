2f12d3c0a8bcb479072c2ea69dd16b9e54775b56
Md Fahad Mia <101195810+fahadewu@users.noreply.github.com>
2023-08-25 20:42:05 +0000

    diagnoseing the issue

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ import express from 'express';
 import router from './routes/userRoutes.js';
 import cors from 'cors';
 const app = express();
-
-app.use('/', router);
+const router = express.Router()
+//app.use('/', router);
 
 app.use(cors("*"));
 
+router.get("/",(req,res)=>{
+    res.send("This is a test response")
+})
 
 app.listen();
